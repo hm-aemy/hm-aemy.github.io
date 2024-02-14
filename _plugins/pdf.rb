321177c956080ee4ece2c21f5957be6f92b5d807
Stefan Wallentowitz <stefan.wallentowitz@hm.edu>
2024-02-14 07:44:11 +0100

    PDF paths and only on changes

--- _plugins/pdf.rb
+++ _plugins/pdf.rb
@@ -1,20 +1,31 @@
 
 
 
-Jekyll::Hooks.register :posts, :post_write do |post|
+Jekyll::Hooks.register :posts, :pre_render do |post|
     # code to call after Jekyll renders a page
        
     if post.data["layout"] == "job"
+        post.data["pdf"] = "/jobs/" + post.basename_without_ext + ".pdf"
+    end
+end
+
+Jekyll::Hooks.register :posts, :post_write do |post|
+    # code to call after Jekyll renders a page
+    if post.data["layout"] == "job"
         qrcode = post.site.config["url"] + post.url
-        htmlfile = post.site.config["destination"] + post.url
-        pdffile = File.join(File.dirname(htmlfile), File.basename(htmlfile, File.extname(htmlfile))+".pdf")
+
+        pdffile = post.data["pdf"]
 
         Jekyll.logger.debug "Generate pdf:", pdffile
 
-        cmd = "pandoc #{post.path} --output #{pdffile} --template jobposting.tex -M qrcode='#{qrcode}'"
+        cmd = "pandoc #{post.path} --output #{File.join(post.site.config["destination"], pdffile)} --template jobposting.tex -M qrcode='#{qrcode}'"
 
         Dir.chdir(post.site.config["plugins_dir"]){
-            %x[#{cmd}] unless File.exists?(pdffile)
+            %x[#{cmd}] unless File.exists?(File.join(post.site.config["destination"], pdffile))
         }
     end
+end
+
+Jekyll::Hooks.register :site, :pre_render do |site|
+    Jekyll.logger.debug "Site:", site.collections["team"]
 end
